da3db9751b2051df7d3446c20730df7323a5c656
sai-Sweshika <saisweshikakodurupaka@gmail.com>
2024-11-13 12:38:00 +0530

    first commit

---
+++ day17.js
@@ -0,0 +1,63 @@
+// let a=new Date();
+// console.log(a);
+// console.log(a.getFullYear());
+// console.log(a.getSeconds());
+// o/p:Wed Nov 13 2024 11:17:12 GMT+0530 (India Standard Time)
+//         2024
+//         12
+// let alarm
+// function alarm ring(){
+// let alarm Time=new Date();
+// alarm Time.setHours(12);
+// alarm Time.setMinutes(55)
+// alarm Time.setSeconds(0);
+// let Time diff=alarm Time-present Time;
+// console.log(time diff);
+// setTimeout(()=>){
+//     alert("your alarm is ringing")
+// }(time diff)
+// console.log("your alarm will be ring at 12");
+// }
+// alarm()
+
+// let a=4.25
+// console.log(Math.round(a));
+// o/p:4
+
+// let a=5.5534
+// console.log(Math.ceil(a));
+// o/p:6
+
+// let a=23.876
+// console.log(Math.floor(a));
+// o/p:23
+
+// let a=163
+// console.log(Math.sqrt(a));
+// o/p:12.767145334803704
+
+// let a=169
+// console.log(Math.sqrt(a));
+// o/p:13
+
+// let a=5
+// console.log(Math.pow(a,5));
+// o/p:3125
+
+// let a=-12
+// console.log(Math.abs(a));
+// o/p:12
+
+// console.log(Math.min(1,2,3,4,5,6,));
+// 0/p:1
+
+// console.log(Math.random()*100);
+// o/p:46.286760760440075
+
+// console.log(Math.floor(Math.random()*100));
+// o/p:58
+
+
+
+
+
